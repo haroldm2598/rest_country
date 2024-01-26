fd86aa0ad9a52292b44dce1baba58794a05025a2
Harold <cvuyante24@gmail.com>
2024-01-26 14:06:02 +0800

    fix flex row

--- src/page/Country.jsx
+++ src/page/Country.jsx
@@ -31,7 +31,7 @@ function Country() {
 	return (
 		<section className='bg-slate-50 py-10'>
 			<div className='max-w-7xl mx-auto px-5 xl:px-0 flex flex-col gap-10'>
-				<div className='flex flex-col lg:flex-row lg:justify-between gap-2 lg:gap-0'>
+				<div className='flex flex-col md:flex-row md::justify-between gap-2 lg:gap-0'>
 					<Input
 						placeholder='Search Country...'
 						handleInput={(e) => dispatch(selectCountry(e.target.value))}
